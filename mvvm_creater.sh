1f8d26e46f8ce1e44381b44dd9b72c462fed2ce7
thenishchalraj <shivamedward@gmail.com>
2021-01-22 00:04:20 +0530

    [removed] unwanted imports

--- mvvm_creater.sh
+++ mvvm_creater.sh
@@ -63,32 +63,12 @@ cd api/
 cat << EOF >> MainApi.kt
 package $3.data.api
 
-import $3.utils.Endpoints
-import com.google.gson.JsonObject
-import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
-
 interface MainApi {}
 EOF
 cat << EOF >> MainDataSource.kt
 package $3.data.api
 
 import android.app.Application
-import androidx.lifecycle.MediatorLiveData
-import $3.R
-import $3.data.model.WeatherModel
-import $3.utils.ISTDateDeserializer
-import $3.utils.Resource
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonObject
-import com.google.gson.reflect.TypeToken
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.util.*
-import kotlin.collections.ArrayList
 
 class MainDataSource(
     private val mainApi: MainApi,
