ab5f4d482ee0ab627616dae11f715d85ade36d72
thenishchalraj <shivamedward@gmail.com>
2021-01-27 20:42:52 +0530

    [modified] activity builders module

--- mvvm_creater.sh
+++ mvvm_creater.sh
@@ -245,7 +245,18 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
-abstract class ActivityBuildersModule {}
+abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector(
+        modules = [
+		/** uncomment the below if you use fragments builder module (recommended) */
+		// FragmentBuildersModule::class,
+		ViewModelModule::class, MainModule::class
+        ]
+    )
+	/** uncomment and check with your own main activity */
+	// abstract fun contributeMainActivity(): MainActivity
+}
 EOF
 cat << EOF >> MainModule.kt
 package $3.di
